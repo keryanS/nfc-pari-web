454c2ef269a8f20761ec8ca2832c75949b168c41
Keryan SANIE <keryan.sanie@gmail.com>
2017-03-02 13:30:09 +0100

    first commit

---
+++ CoreAdmin/js/api.js
@@ -0,0 +1,81 @@
+jQuery(function($){
+
+    var apiUrl = "http://localhost/";
+
+    function getSurveys(category){
+        if(category != null){
+            category = "/" + category;
+        }
+        else {
+            category = "";
+        }
+        $.ajax({
+            url: apiUrl+"get/surveys/",
+        }).done(function(results) {
+            return results.data;
+        }).fail(function() {
+            return "error getting surveys" ;
+        });
+    }
+
+    function getCurrentSurvey(){
+        $.ajax({
+            url: apiUrl+"get/survey/current",
+        }).done(function(results) {
+            return results.data;
+        }).fail(function() {
+            return "error getting surveys" ;
+        });
+    }
+
+    function getSurvey(id){
+        $.ajax({
+            url: apiUrl+"get/survey/"+id,
+        }).done(function(results) {
+            return results.data;
+        }).fail(function() {
+            return "error getting surveys" ;
+        });
+    }
+
+    function getSurveysCharts(){
+        $.ajax({
+            url: apiUrl+"get/surveys/charts",
+        }).done(function(results) {
+            return results.data;
+        }).fail(function() {
+            return "error getting surveys" ;
+        });
+    }
+
+    function getSurveyChart(id){
+        $.ajax({
+            url: apiUrl+"get/survey/"+id+"/charts",
+        }).done(function(results) {
+            return results.data;
+        }).fail(function() {
+            return "error getting surveys" ;
+        });
+    }
+
+    function getTop10Players(){
+        $.ajax({
+            url: apiUrl+"get/users/top10",
+        }).done(function(results) {
+            return results.data;
+        }).fail(function() {
+            return "error getting players" ;
+        });
+    }
+
+    function getFlop10Players(){
+        $.ajax({
+            url: apiUrl+"get/users/flop10",
+        }).done(function(results) {
+            return results.data;
+        }).fail(function() {
+            return "error getting players" ;
+        });
+    }
+
+});
---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Paris sportif</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+		
+</head>
+<body>
+
+<div id="main-versus">
+
+	<div class="zone1"><p style="color:red"><?php echo 'mes couilles';?></p></div>
+	<div class="zone2"></div>
+</div>
+</body>
+</html>
